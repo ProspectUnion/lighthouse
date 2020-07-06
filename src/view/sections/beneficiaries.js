8f217628c35af706faa819dc8bddc5bcd3a97a1c
ProspectUnion <60887363+ProspectUnion@users.noreply.github.com>
2020-07-06 10:17:59 +0100

    Update beneficiaries.js

--- src/view/sections/beneficiaries.js
+++ src/view/sections/beneficiaries.js
@@ -15,17 +15,17 @@ const Sections = {
             "We regularly consult with beneficiaries on how the project affects them and how their interests have evolved.",
             "Our project is co-created with our beneficiaries."
         ],
-        copy: [  
+        copy: [
             m("p", "Your project likely exists to help improve the working lives of a group or groups of workers--your beneficiaries. These could be your members, or a broader set of workers, or some other community."),
-            m("p", "Ideally, your project’s beneficiaries wouldn’t just play a passive role: they would be involved in both governance and operations. This could range from having beneficiaries on boards and committees, to holding regular consultation and feedback sessions."),
+            m("p", "Ideally, your project’s beneficiaries wouldn’t just play a passive role: they would be involved in both governance and operations. This could range from having beneficiaries on committees and other groups, to holding regular consultation and feedback sessions."),
         ],
         advice: [
             m("p", `Keeping beneficiaries, such as your members, involved in your project is a good way to ensure that the project continues to work for them. At a minimum, you should find ways to regularly consult with them, and keep them informed. Not only will this feedback help your project, but it can build stronger bonds between your team and your members. Not everyone will have the same amount of time or energy to help with your project. Find different ways for workers to engage, such as: `),
             m("ul", [
-                m("li", `Workers could sit on a data advisory council, which could serve as a board of directors for the project;`),
+                m("li", `Workers could sit on a data advisory group, which could serve as an oversight body for the project;`),
                 m("li", `You could issue surveys to your broader membership on how the project is going;`),
                 m("li", `Members could participate in a focus group;`),
-                m("li", `Project champions could engage directly with their peers about the project; `)
+                m("li", `Project champions could engage directly with their fellow workers about the project; `)
 
             ])
         ]
@@ -40,19 +40,19 @@ const Sections = {
             "We have a conflict of interest policy, and require internal and external stakeholders to adhere to it.",
         ],
         levels: [0,null,1,2,3],
-        copy: [  
-            m("p", "Your project likely involves an ecosystem of partners: from other unions to third-party organizations, vendors, academic researchers, and government regulators."),
-            m("p", "Each of these partners has something to lose or gain from your project, and their interests may not always align with yours. Good projects account for this—with legal agreements to ensure good behavior, with documented internal practices to comply with regulations, with participation from beneficiaries to ensure their interests are being looked after."),
+        copy: [
+            m("p", "Your project likely involves an ecosystem of partners: from other unions to third-party organisations, employers, suppliers, academic researchers, and government regulators."),
+            m("p", "Each of these partners has something to lose or gain from your project, and their interests may not always align with yours. Good projects account for this—with legal agreements to ensure good behaviour, with documented internal practices to comply with regulations, with participation from beneficiaries to ensure their interests are being looked after."),
         ],
         advice: [
-           m("p", `Your project may depend on external partners, including vendors. Although this is totally normal, external partners may not share your priorities or your project's, especially when it comes to handling data. For each technology partnership you build, ensure that there is a safe way to wind the project down that protects or deletes worker data. This could include setting a process for transferring your data to another platform or project.`),
+           m("p", `Your project may depend on external partners, including suppliers or vendors. Although this is totally normal, external partners may not share your priorities or your project's, especially when it comes to handling data. For each technology partnership you build, ensure that there is a safe way to wind the project down that protects or deletes worker data. This could include setting a process for transferring your data to another platform or project.`),
            m("p", `External partners should be held to data handling standards that are just as stringent as the ones your team operates under--if not more so. Consider including clauses in your partnership contracts that prohibit uses of your project's data beyond those that are explicitly specified in the contract, or for any purpose that does not solely benefit your project's community. This small step can make a tremendous difference.`),
-           m("p", `Finally,  your project and your organization needs a conflict of interest policy. External partners should be vetted for conflicts between their interests and the goals of the project, even if it doesn't directly implicate their involvement in the project.`)
+           m("p", `Finally,  your project and your organisation needs a conflict of interest policy. External partners should be vetted for conflicts between their interests and the goals of the project, even if it doesn't directly bar their involvement in the project.`)
         ]
     },
-   
 
-    
+
+
 }
 
 const Beneficiaries = {
